e3d9c008878aa45e25db22846be09e7d95431c90
Keenan <keenanhallas@gmail.com>
2020-06-18 20:31:58 -0400

    forgot to save some files

---
+++ reducer-todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+
+}
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        case "":
+            //code here
+        default "":
+            //code here
+    }
+}
